03460b16407980f5545412287c750bcb0240d280
sowon-bak <cruzean479@gmail.com>
2025-09-26 15:49:36 +0900

    Delete day2 directory

--- day2/schemas.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields
-
-class BookSchema(Schema):
-    id = fields.Int(dump_only=True)
-    title = fields.String(required=True)
-    author = fields.String(required=True)
